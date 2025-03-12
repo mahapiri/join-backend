cdbaff089aa91e84bdd210c2aafa7d3602508d1c
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-03-12 12:37:05 +0100

    added task model and added admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from tasks.models import AssignedTo, Subtask, Task
+
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'prio', 'created_date', 'due_date')
+    search_fields = ('title', 'prio')
+    list_filter = ('prio',)
+
+class SubtaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'title')
+    search_fields = ('task', 'title')
+    list_filter = ('task',)
+
+class AssignedToAdmin(admin.ModelAdmin):
+    list_display = ('task', 'profile')
+    search_fields = ('task', 'profile')
+    list_filter = ('task',)
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Subtask, SubtaskAdmin)
+admin.site.register(AssignedTo, AssignedToAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from profiles.models import Profile
+
+PRIO_CHOICES = [
+    ("urgent", "Urgent"),
+    ("medium", "Medium"),
+    ("low", "Low"),
+]
+
+CATEGORY_CHOICES = [
+    ("technical_task", "Technical Task"),
+    ("user_story", "User Story"),
+]
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField()
+    due_date = models.DateField(null=False, blank=False)
+    prio = models.CharField(max_length=10, choices=PRIO_CHOICES, default="low")
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, null=False, blank=False, default="user_story")
+    created_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.title
+    
+    def get_assigned_to(self):
+        profiles = [assignment.profile for assignment in self.assignedto_set.all()]
+        return ", ".join(str(profile) for profile in profiles) if profiles else "Keine Profile vorhanden"
+
+    def get_subtasks(self):
+        return ", ".join(subtask.title for subtask in self.subtasks.all()) if self.subtasks.exists() else "keine Subtasks"
+
+class AssignedTo(models.Model):
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name="assignments")
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="assignments")
+
+    class Meta:
+        verbose_name = "Assigned To"
+        verbose_name_plural = "Assigned To"
+    
+    def __str__(self):
+        return f"{self.task.title}: {self.profile}"
+
+class Subtask(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="subtasks")
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Subtask"
+        verbose_name_plural = "Subtasks"
+
+    def __str__(self):
+        return f"{self.task.title}: {self.title}"
