caa515f175814726e30228c42aad2a07fe330e13
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-03-19 11:25:25 +0100

    edited description func in taskserializer

--- tasks/api/serializers.py
+++ tasks/api/serializers.py
@@ -6,7 +6,7 @@ from tasks.models import CATEGORY_CHOICES, PRIO_CHOICES, STATUS_CHOICES, Subtask
 class TasksSerializer(serializers.ModelSerializer):
     status = serializers.ChoiceField(choices=STATUS_CHOICES, default="to_do")
     title = serializers.CharField(max_length=255)
-    description = serializers.CharField(required=False, allow_blank=True)
+    description = serializers.CharField(required=False, allow_blank=True, allow_null=True, default="")
     due_date = serializers.DateField(required=True)
     prio = serializers.CharField(required=False, allow_blank=True, default="low")
     category = serializers.ChoiceField(choices=CATEGORY_CHOICES)
@@ -20,6 +20,9 @@ class TasksSerializer(serializers.ModelSerializer):
     
     def validate_prio(self, value):
         return value if value else "low"
+    
+    def validate_description(self, value):
+        return value if value is not None else ""
     
 class SubtaskSerializer(serializers.ModelSerializer):
     task_title = serializers.CharField(source="task.title", read_only=True)
