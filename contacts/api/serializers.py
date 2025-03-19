0f8e23a720472638a8a5059c859873ca064d1510
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-03-19 21:05:44 +0100

    edited

--- contacts/api/serializers.py
+++ contacts/api/serializers.py
@@ -10,7 +10,7 @@ class ContactsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Contact
-        fields = ['name', 'email', 'phone']
+        fields = '__all__'
 
     def create(self, validated_data):
         return Contact.objects.create(**validated_data)
